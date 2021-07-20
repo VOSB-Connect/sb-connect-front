c7ccab9d2b773cfc0dc2a949c840279f7bdbf5b6
Unknown <blossomperez18@gmail.com>
2021-07-19 19:41:39 -0500

    Add cage component to registration

--- src/routes/registration.svelte
+++ src/routes/registration.svelte
@@ -1,56 +1,73 @@
 <script>
-    import { useForm, Hint, HintGroup, validators, minLength } from "svelte-use-form"
+    // import { useForm, Hint, HintGroup, validators, minLength } from "svelte-use-form"
+    import Cage from '$lib/components/cage.svelte'
 
-    let companyName = "testCompany";
-    let email = "sample@example.com";
-    let password = "abc123";
-    let duns = "12345";
-    let message = "";
-    let didRegister = true
-    let confirmed = true;
-    let error;
+    // let companyName = "testCompany";
+    // let email = "sample@example.com";
+    // let password = "abc123";
+    // let cage = "12345";
+    // let message = "";
+    // let didRegister = true
+    // let confirmed = true;
+    // let error;
+    let cageCode;
+    
 
-    const submitForm = async() => {
-        try {
-
-            const registrationHeaders = new Headers();
-            registrationHeaders.append("Content-Type", "application/json")
-            const registrationInfo = JSON.stringify({
-                    email,
-                    companyName,
-                    password,
-                    username: duns,
-                    confirmed
-            });
-            const requestOptions = {
-                method: "POST",
-                headers: registrationHeaders,
-                body: registrationInfo,
-                redirect: 'follow'
-            };
-            
-            const register = await fetch("http://localhost:1337/auth/local/register", requestOptions);
-            
-            if(register.ok){
-                const registerResponse = await register.json()
-                didRegister = true;
-                console.log(registerResponse)
-            } else {
-                const failedResponse = await register.json()
-                didRegister = false;
-                error = failedResponse.message[0].messages[0].message;
-            }
-            
-        } catch (err) {
-            error = err
-        }    
+    async function getInformation(){
+        // let response = await fetch(`https://api.sam.gov/entity-information/v1/entities?api_key=r3cYNK8ZhHkddQ6mX6Km8PqZG8JBqKqhWWWyLmlL&cageCode=${ cageCode }`)
+        // let data = await response.json()
+        // data.then(console.log)
+        console.log(cageCode)
     }
+
+
+    // const submitForm = async() => {
+    //     try {
+
+    //         const registrationHeaders = new Headers();
+    //         registrationHeaders.append("Content-Type", "application/json")
+    //         const registrationInfo = JSON.stringify({
+    //                 email,
+    //                 companyName,
+    //                 password,
+    //                 username: cage,
+    //                 confirmed
+    //         });
+    //         const requestOptions = {
+    //             method: "POST",
+    //             headers: registrationHeaders,
+    //             body: registrationInfo,
+    //             redirect: 'follow'
+    //         };
+            
+    //         const register = await fetch("http://localhost:1337/auth/local/register", requestOptions);
+            
+    //         if(register.ok){
+    //             const registerResponse = await register.json()
+    //             didRegister = true;
+    //             console.log(registerResponse)
+    //         } else {
+    //             const failedResponse = await register.json()
+    //             didRegister = false;
+    //             error = failedResponse.message[0].messages[0].message;
+    //         }
+            
+    //     } catch (err) {
+    //         error = err
+    //     }    
+    // }
 
 
 </script>
 
 <main>
-        {#if message == ""} 
+
+    <Cage getInformation={ getInformation } code={ cageCode }></Cage>
+
+
+
+
+        <!-- {#if message == ""} 
             {#if !didRegister}
                 <p>{error}</p>
             {/if}
@@ -74,7 +91,7 @@
             </form>
         {:else if didRegister}
             <p>Verification email has been sent to {email}. Please check email to varify.</p>
-        {/if}
+        {/if} -->
 </main>
 
 
