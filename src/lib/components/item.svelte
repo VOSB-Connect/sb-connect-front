bd06397b6847e6c368501f7145d89cc00a324010
Unknown <blossomperez18@gmail.com>
2021-07-23 17:56:26 -0500

    Add bind value as prop and correct post request

--- src/lib/components/item.svelte
+++ src/lib/components/item.svelte
@@ -1,6 +1,7 @@
 <script>
     export let title="";
     export let userItem="";
+    export let bindValue="";
 </script>
 
 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -8,5 +9,6 @@
       { title }
     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
       { userItem }  
+      <input bind:value={ bindValue } type="hidden">
     </dd>
   </div>
