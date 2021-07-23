d570e06be25918f139f71aa551352258a48ce30f
Unknown <blossomperez18@gmail.com>
2021-07-23 17:54:59 -0500

    Refactor registration page

--- src/routes/registration.svelte
+++ src/routes/registration.svelte
@@ -16,39 +16,8 @@
     {:else }
         <Verify bind:userInformation={ userInformation } />
     {/if}
-
-
-    <!-- {#if message == ""} 
-            {#if !didRegister}
-                <p>{error}</p>
-            {/if}
-            <form on:submit|preventDefault={ submitForm }>
-                <label for="companyName">Company Name:</label>
-                <input type="text" id="companyName" name="companyName" bind:value={ companyName } >
-
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" bind:value={ email }>
-
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" bind:value={ password }>
-
-                <label for="confirm-password">Confirm Password</label>
-                <input type="password" id="confirm-password" name="confirm-password">
-                
-                <label for="duns-number">DUNS #</label>
-                <input type="text" id="duns-number" name="duns-number" bind:value={ duns }>
-
-                <input type="submit" value="Register"> 
-            </form>
-        {:else if didRegister}
-            <p>Verification email has been sent to {email}. Please check email to varify.</p>
-        {/if} -->
 </main>
 
 
-<style>
-    label {
-        display: block;
-    }
-</style>
+
 
