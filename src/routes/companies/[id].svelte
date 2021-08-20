f7caaae8304eb2a2f8fb56a6282293bd2278f560
Blossom Perez <blossomperez18@gmail.com>
2021-08-19 23:58:16 -0500

    Create profile biography

--- src/routes/companies/[id].svelte
+++ src/routes/companies/[id].svelte
@@ -11,4 +11,38 @@
 </script>
 
 
-<p>Company id: {id}</p>
+<div class="container mx-auto w-6/12">
+    <div class="text-center">
+        <p class="py-8">Spatialgis, L.L.C id:{id}</p>
+    </div>
+    <div class="container mx-auto flex">
+        <div class="right">
+            <div>
+                <p>Cage: <span></span></p>
+            </div> 
+            <div>
+                <p>DUNS: <span></span></p>
+            </div> 
+            <div>
+                <p>Company Site: <span></span></p>
+            </div> 
+        </div>
+        <div class="left">
+            <div>
+                <p>Statement of Capability: <span></span></p>
+            </div> 
+            <div>
+                <p>Location: <span></span></p>
+            </div> 
+            <div>
+                <p>Primary NAICS: <span></span></p>
+            </div> 
+        </div>
+        </div>
+</div>
+
+<style>
+    div {
+        border: 1px solid black;
+    }
+</style>
