c65620aa1a57415f0dcd7cef2b13df0f9d974fc1
Jason Gutierrez <jasong84@gmail.com>
2021-08-27 15:38:15 -0500

    Style the registration page with a layout

--- src/routes/registration/index.svelte
+++ src/routes/registration/index.svelte
@@ -37,7 +37,7 @@
                 const registerResponse = await register.json()
                 didRegister = true;
                 console.log(registerResponse)
-                goto('/dashboard')
+                goto('/activation')
             } else {
                 const failedResponse = await register.json()
                 didRegister = false;
@@ -87,11 +87,11 @@
 
 </script>
 
-<div class="bg-grey-lighter min-h-screen flex flex-col">
-        <form class="mt-8 space-y-6" on:submit|preventDefault={ getInformation }>
-        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+<div class="min-h-screen flex flex-col justify-center">
+    <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <form class="space-y-6" on:submit|preventDefault={ getInformation }>
             <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+                <h1 class="mb-8 text-3xl text-center">Register for HUB Connection</h1>
                 <input 
                     type="text"
                     class="block border border-grey-light w-full p-3 rounded mb-4"
@@ -117,15 +117,10 @@
                     placeholder="Password" 
                     bind:value={ password }
                 />
-                <!-- <input 
-                    type="password"
-                    class="block border border-grey-light w-full p-3 rounded mb-4"
-                    name="confirm_password"
-                    placeholder="Confirm Password" /> -->
 
                 <button
                     type="submit"
-                    class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                    class="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1"
                 >Create Account</button>
 
                 <div class="text-center text-sm text-grey-dark mt-4">
@@ -138,15 +133,12 @@
                     </a>
                 </div>
             </div>
-
-            <div class="text-grey-dark mt-6">
-                Already have an account? 
-                <a class="no-underline border-b border-blue text-blue" href="/login">
-                    Sign in
-                </a>.
-            </div>
+        </form>
+        <div class="text-white mt-6">
+            Already have an account? 
+            <a class="no-underline border-b border-blue text-blue" href="/login">Sign in</a>.
         </div>
-    </form>
+    </div>
 </div>
 
 
