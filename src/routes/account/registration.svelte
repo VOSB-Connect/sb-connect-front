08109b967c54c185d89761905e52b30d282cffc1
Jason Gutierrez <jasong84@gmail.com>
2021-08-28 16:54:36 -0500

    Update registration page

--- src/routes/account/registration.svelte
+++ src/routes/account/registration.svelte
@@ -1,63 +1,34 @@
 <script>
     import { goto } from '$app/navigation';
+    import { post } from '$lib/utils'
     import {faInfo} from '@fortawesome/free-solid-svg-icons'
     import Fa from "svelte-fa"
+    import ListError from '$lib/ListError.svelte'
 
-    let cageCode = "";
-    let email = "";
-    let password = "";
-    let userInformation = {}
-    let message = "";
-    let didRegister = true
+    let cageCode = "", email = "", password = "", userInformation = {};
     let confirmed = false;
     let match = true;
-    let error;
-
+    let error = null;
     
-    async function submitForm(userInformation) {
+    async function handleRegistration(userInformation) {
         try {
-
-            const registrationHeaders = new Headers();
-            registrationHeaders.append("Content-Type", "application/json")
-            const registrationInfo = JSON.stringify({
-              username: userInformation.cageCode,
-              email,
-              password,
-              confirmed,    
-              ...userInformation,
-            });
-            const requestOptions = {
-                method: "POST",
-                headers: registrationHeaders,
-                body: registrationInfo,
-                redirect: 'follow'
-            };
-            
-            const register = await fetch("http://localhost:1337/auth/local/register", requestOptions);
-            
-            if(register.ok){
-                const registerResponse = await register.json()
-                didRegister = true;
-                console.log(registerResponse)
+            const registrationResponse = await post("auth/local/register", { 
+                username: userInformation.cageCode, email, password, confirmed, ...userInformation,
+            })
+            if(registrationResponse.ok){
+                const registerResponse = await registrationResponse.json()
                 goto('/activation')
             } else {
-                const failedResponse = await register.json()
-                didRegister = false;
-                error = failedResponse.message[0].messages[0].message;
-            }
-            
+                error = registrationResponse.message[0].messages[0].message;
+            }        
         } catch (err) {
-            error = err
             console.error(err)
         }    
     }
 
-    function getInformation(){
+    function handleValidUser(){
         // let response = await fetch(`https://api.sam.gov/entity-information/v1/entities?api_key=r3cYNK8ZhHkddQ6mX6Km8PqZG8JBqKqhWWWyLmlL&cageCode=${ cageCode }`)
-        // let data = await response.json()
-            
-        // const { entityData } = data;
-
+        // const { entityData } = await response.json()
         // userInformation = {
         //     legalBusinessName: entityData[0].entityRegistration.legalBusinessName,
         //     duns: entityData[0].entityRegistration.ueiDUNS,
@@ -80,7 +51,7 @@
         }
 
         if(email === userInformation.samEmail) {
-            submitForm(userInformation);
+            handleRegistration(userInformation);
         }else {
             match = false;
         }
@@ -96,23 +67,27 @@
         </a>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Register</h2>
         <p class="mt-2 text-center text-sm text-gray-600">Already have an account? <a href="/account/login" class="font-medium text-indigo-600 hover:text-indigo-500">Log in</a></p>
-        <form class="mt-8 space-y-6" on:submit|preventDefault={ getInformation }>
+        <form class="mt-8 space-y-6" on:submit|preventDefault={ handleValidUser }>
             {#if !match} 
-                <div class=" rounded flex items-center gap-x-2 bg-red-700 text-white text-sm font-bold p-3" role="alert">
-                    <Fa icon={faInfo} size="md" /> <p>Email does not match sam.gov email</p>
-                </div>           
+                <ListError error={"The e-mail entered does not match the e-mail on <a class='link pointer' href='https://sam.gov/'>sam.gov</a></span>"} />    
             {/if}
             <div>
                 <label for="cageCode" class="sr-only">Cage Code</label>
-                <input type="text" class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" name="cageCode" placeholder="Cage Code" bind:value={ cageCode } />
+                <input type="text" bind:value={ cageCode } name="cageCode" placeholder="Cage Code"
+                    class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
             </div>
             <div>
                 <label for="email" class="sr-only">Email address</label>
-                <input type="email" class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" name="email" placeholder="Email" bind:value={ email } />
+                <input type="email" bind:value={ email } name="email" placeholder="Email"
+                    class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
             </div>
             <div>
                 <label for="password" class="sr-only">Password</label>
-                <input type="password" class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" name="password" placeholder="Password" bind:value={ password } />
+                <input type="password" bind:value={ password } name="password" placeholder="Password" 
+                    class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
             </div>
             <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Create Account
