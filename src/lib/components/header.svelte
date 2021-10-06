aa601e2c6bac5abff874a36cd6b15b0233ffff3f
Cpt_Crunch <jasong84@gmail.com>
2021-10-05 19:02:18 -0500

    Update button href to registration page

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -8,7 +8,7 @@
             <h2 class="mt-6 text-3xl font-bold text-indigo-300 md:text-5xl">Small Business. Big Results.<br>Connections Simplified.</h2>
             <div class="flex justify-center mt-8">
                 <a class="px-8 py-2 text-lg font-medium text-yellow-600 transition-colors duration-300 transform bg-yellow-400 rounded hover:bg-indigo-500"
-                    href="/register">Register Today</a>
+                    href="/account/registration">Register Today</a>
             </div>
         </div>
     </section>
