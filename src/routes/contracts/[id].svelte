c69220235a62cf62e06eb473f27514405f3626af
Unknown <blossomperez18@gmail.com>
2021-10-19 20:20:41 -0500

    Create contract page

---
+++ src/routes/contracts/[id].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    export async function load({ page }) {
+        const contractId = page.params.id;
+        return {
+            props: { contractId }
+        };
+    }
+</script>
+<script>
+    import { get } from '$lib/utils';
+    export let companyId;
+
+    async function getContract() {
+        const response = await get(`contracts/${ contractId }`)
+        if(response.ok) return response.json();
+    }
+</script>
+
+{#await getContract() then contract}
+    
+{/await}
