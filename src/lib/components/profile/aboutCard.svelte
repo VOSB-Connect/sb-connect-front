a7eaa8ba349905b6fecdae133adaa8bf4617c9fb
Donelle Harris <donelle.harris@gmail.com>
2021-10-21 22:45:24 -0500

    Update About Card
    add Point of Contact information
    layout adjustments to fit data

--- src/lib/components/profile/aboutCard.svelte
+++ src/lib/components/profile/aboutCard.svelte
@@ -2,8 +2,9 @@
 	export let business;
 	const {addressLine1, addressLine2, city, stateOrProvinceCode, zipCode} = business.coreData.mailingAddress;
 
+	let pointOfContact = business.assertions.pointsOfContact?.electronicBusinessPOC;
+	pointOfContact = pointOfContact == null ? {}:pointOfContact;
 </script>
-
 
 <div class="bg-white p-3 shadow-sm rounded-sm col-span-2">
 	<div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
@@ -14,38 +15,35 @@
 					d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
 			</svg>
 		</span>
-		<span class="tracking-wide">About</span>
+		<span class="tracking-wide">Contact</span>
 	</div>
 	<div class="text-gray-700">
-		<div class="grid md:grid-cols-2 text-sm">
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">First Name</div>
-				<div class="px-4 py-2">Jane</div>
+		<div class="grid md:grid-cols text-sm">
+			<div class="grid grid-cols-3">
+				<div class="px-4 py-2 font-semibold">Name</div>
+				<div class="px-4 py-2 col-span-2">{pointOfContact.firstName ?? "No Point of Contact listed"} {pointOfContact.lastName ?? ""}</div>
 			</div>
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">Last Name</div>
-				<div class="px-4 py-2">Doe</div>
-			</div>
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">Gender</div>
-				<div class="px-4 py-2">Female</div>
-			</div>
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">Contact No.</div>
-				<div class="px-4 py-2">+11 998001001</div>
-			</div>
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">Mailing Address</div>
-				<div class="px-4 py-2">{addressLine1} {addressLine2} {city}, {stateOrProvinceCode} {zipCode}</div>
-			</div>
-			<div class="grid grid-cols-2">
-				<div class="px-4 py-2 font-semibold">Email.</div>
-				<div class="px-4 py-2">
-					<a class="text-blue-800" href="mailto:jane@example.com">jane@example.com</a>
+			<div class="grid grid-cols-3">
+				<div class="px-4 py-2 font-semibold">Email</div>
+				<div class="px-4 py-2 col-span-2">
+					{#if pointOfContact.email }
+						<a class="text-blue-800" href="mailto:{pointOfContact.email}">{pointOfContact.email}</a>
+						{:else} 
+						<p>No Point of Contact email listed.</p>
+					{/if}
+					
 				</div>
 			</div>
+			<div class="grid grid-cols-3">
+				<div class="px-4 py-2 font-semibold">Contact No.</div>
+				<div class="px-4 py-2 col-span-2">{pointOfContact.usPhone ?? ""}</div>
+			</div>
+			<div class="grid grid-cols-3">
+				<div class="px-4 py-2 font-semibold">Mailing Address</div>
+				<div class="px-4 py-2 col-span-2">{addressLine1} {addressLine2 ?? ""} {city}, {stateOrProvinceCode} {zipCode}</div>
+			</div>
+			
 		</div>
 	</div>
-	<button class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Show
-		Full Information</button>
+	
 </div>
