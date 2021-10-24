e9b8d3ab2d00decb1641cc6779aba2994c766b76
Donelle Harris <donelle.harris@gmail.com>
2021-10-23 21:44:40 -0500

    Add partner cards to saved partners page
    create cards on partnerCard.svelte
    create function to pull partner list (index.svelte)

--- src/routes/dashboard/partners/__layout.svelte
+++ src/routes/dashboard/partners/__layout.svelte
@@ -3,18 +3,5 @@
 </script>
 <!-- main content -->
 <main class="flex-grow flex flex-col min-h-0 border-t">
-    <section class="flex justify-center flex-wrap">
-        <!-- Partner Card -->
-        <div class="w-full m-2 md:w-64 justify-center items-center bg-white shadow-lg rounded-lg flex flex-col">
-            <div class="w-full p-4 justify-center flex flex-col">
-                <h4 class="border-b-2 text-3xl" id="whoobe-3mr7n">Business Name Here</h4>
-                <p class="my-1" >Contact name</p>
-                <p class="my-1" >Contact email</p>
-                <p class="my-1" >Contact address???</p>
-                <p class="my-1" >Comma separated business types list</p>
-                <p class="my-1" >Comma separated NAICs code list</p>
-                <button value="button" class="my-4 px-4 py-2 text-white hover:bg-blue-700 bg-blue-500" id="whoobe-jkkr2">View Full Business Profile</button>
-            </div>
-        </div>
-    </section>
+    
 </main>
--- src/routes/dashboard/partners/index.svelte
+++ src/routes/dashboard/partners/index.svelte
@@ -1,3 +1,16 @@
 <script>
+    import PartnerCard from "$lib/components/partners/partnerCard.svelte";
 
+    async function getMyPartners(){
+        const response = await get(`partners/myPartners`);
+        if(response.ok) return response.json();
+    }
 </script>
+<main class="flex-grow flex flex-col min-h-0 border-t">
+    {#await getMyPartners() then partners}
+        {#each partners as partner}
+            <PartnerCard {partner}/>
+        {/each}
+    
+    {/await}
+</main>
