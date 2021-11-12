5c69136ac0aabf4df8d79c0386e2faa0f646e63e
Donelle Harris <donelle.harris@gmail.com>
2021-11-11 21:00:10 -0600

    Add Search feature to Dashboard

--- src/lib/components/dashboard/charts.svelte
+++ src/lib/components/dashboard/charts.svelte
@@ -1,7 +1,7 @@
 <!--- FIRST ROW CONTAINING THE  STATS CARD STARTS HERE -->
   <div class="flex bg-gray-100 py-10 p-14">
-<!--- First Stats Container --->
-    <div class="container mx-auto pr-4">
+<!--- First Container --->
+    <div class="container ml-0 pr-4">
       <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
         <div class="h-20 bg-red-400 flex items-center justify-between">
           <p class="mr-0 text-white text-lg pl-5">TOTAL OPPORTUNITIES</p>
@@ -14,7 +14,7 @@
         <!-- <hr > -->
       </div>
     </div>
-<!--- Second Stats Container --->
+<!--- Second Container --->
     <!-- <div class="container mx-auto pr-4">
       <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
         <div class="h-20 bg-blue-500 flex items-center justify-between">
@@ -27,7 +27,7 @@
         <hr >
       </div>
     </div> -->
-<!--- Third Stats Container --->
+<!--- Third Container --->
     <!-- <div class="container mx-auto pr-4">
       <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
         <div class="h-20 bg-purple-400 flex items-center justify-between">
@@ -40,7 +40,7 @@
         <hr >
       </div>
     </div> -->
-<!--- Fourth Stats Container --->
+<!--- Fourth Container --->
     <!-- <div class="container mx-auto">
       <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
         <div class="h-20 bg-purple-900 flex items-center justify-between">
@@ -82,151 +82,5 @@
         </tbody>
       </table>
     </div>
-<!-- first div begins here  -->
-  <!-- <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
-    <div class="w-11/12 mx-auto">
-      <div class="bg-white my-6">
-        <table class="text-left w-full border-collapse"> 
-          <thead>
-            <tr>
-              <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">KEYWORDS</th>
-              <th class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">TOTAL ENTERIES</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="hover:bg-grey-lighter">
-              <td class="py-4 px-6 border-b border-grey-light">Bible</td>
-              <td class="py-4 px-6 text-center border-b border-grey-light">
-                11980
-              </td>
-            </tr>
-            <tr class="hover:bg-grey-lighter">
-              <td class="py-4 px-6 border-b border-grey-light">Blah</td>
-              <td class="py-4 px-6 text-center border-b border-grey-light">
-                340
-              </td>
-            </tr>
-            <tr class="hover:bg-grey-lighter">
-              <td class="py-4 px-6 border-b border-grey-light">Blah</td>
-              <td class="py-4 px-6 text-center border-b border-grey-light">
-                901
-              </td>
-            </tr>
-            <tr class="hover:bg-grey-lighter">
-              <td class="py-4 px-6 border-b border-grey-light">Blah</td>
-              <td class="py-4 px-6 text-center border-b border-grey-light">
-                11950
-              </td>
-            </tr>
-            <tr class="hover:bg-grey-lighter">
-              <td class="py-4 px-6 border-b border-grey-light">Blah</td>
-              <td class="py-4 px-6 text-center border-b border-grey-light">
-                459
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div> -->
-<!-- first div ends here --->
-
-<!-- Second div begins here --->
-    <!-- <div class="container mr-5 mx-auto bg-white shadow-xl">
-      <div class="w-11/12 mx-auto">
-        <div class="bg-white my-6">
-          <table class="text-left w-full border-collapse">
-            <thead>
-              <tr>
-                <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">MSISDN</th>
-                <th class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">ENTRIES</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">26809304030</td>
-                <td class="py-4 px-6 text-center border-b border-grey-light">
-                  495,455
-                </td>
-              </tr>
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">26809304030</td>
-                <td class="py-4 px-6 text-center border-b border-grey-light">
-                  495,455
-                </td>
-              </tr>
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">26809304030</td>
-                <td class="py-4 px-6 text-center border-b border-grey-light">
-                  495,455
-                </td>
-              </tr>
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">26809304030</td>
-                <td class="py-4 px-6 text-center border-b border-grey-light">
-                  32,333
-                </td>
-              </tr>
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">26809304030</td>
-                <td class="py-4 px-6 text-center border-b border-grey-light">
-                  31,199
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        </div>
-          </div> -->
-<!-- Second div ends here --->
-
-<!-- Third div begins here --->
-      <!-- <div class="container mx-auto bg-white shadow-xl">
-        <div class="w-11/12 mx-auto">
-          <div class="bg-white my-6">
-            <table class="text-left w-full border-collapse">
-              <thead>
-                <tr>
-                  <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">MSISDN</th>
-                  <th class="py-4 px-6 text-center bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">POINTS</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">28679609009</td>
-                  <td class="py-4 text-center px-6 border-b border-grey-light">
-                    11,290
-                  </td>
-                </tr>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">28679609009</td>
-                  <td class="py-4 text-center px-6 border-b border-grey-light">
-                    9,230
-                  </td>
-                </tr>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">28679609009</td>
-                  <td class="py-4 px-6 text-center border-b border-grey-light">
-                    234
-                  </td>
-                </tr>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">28679609009</td>
-                  <td class="py-4 px-6 text-center border-b border-grey-light">
-                    56,230
-                  </td>
-                </tr>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">28679609009</td>
-                  <td class="py-4 px-6 text-center border-b border-grey-light">
-                    323
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div> -->
-<!-- Third div ends here --->
   </div>
 <!-- SECOND ROW CONTAINING THE TEN MOST RECENT CONTRACT OPPORTUNITIES ENDS HERE -->
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import DashboardStatistics from '$lib/components/dashboard/statistics.svelte'
   import Charts from '$lib/components/dashboard/charts.svelte'
+  import DashboardSearchResults from '$lib/components/dashboard/searchResults.svelte'
 </script>
 
 <!-- main content -->
@@ -12,4 +13,11 @@
   <section aria-label="main content" class="flex min-h-0 flex-col flex-auto border-l">
     <Charts />
   </section>
+  
+  <!-- section content -->
+  <section aria-label="main content" class="flex min-h-0 flex-col flex-auto border-l">
+		<!-- section body header -->
+		
+		<DashboardSearchResults />
+	</section>
 </main>
