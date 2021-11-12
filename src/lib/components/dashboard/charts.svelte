f427ba91154620460cd2ee2ce64ec6a8ba568a92
Donelle Harris <donelle.harris@gmail.com>
2021-11-11 18:17:17 -0600

    Start cleanup of Dashboard
    Removes Stats section
    Content changes to charts section
    Adds "Ten Most Recent" section

--- src/lib/components/dashboard/charts.svelte
+++ src/lib/components/dashboard/charts.svelte
@@ -1,74 +1,91 @@
-<!---===================== FIRST ROW CONTAINING THE  STATS CARD STARTS HERE =============================-->
+<!--- FIRST ROW CONTAINING THE  STATS CARD STARTS HERE -->
   <div class="flex justify-center bg-gray-100 py-10 p-14">
-    <!---== First Stats Container ====--->
-  <div class="container mx-auto pr-4">
-    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
-      <div class="h-20 bg-red-400 flex items-center justify-between">
-        <p class="mr-0 text-white text-lg pl-5">BT SUBSCRIBERS</p>
+<!--- First Stats Container --->
+    <div class="container mx-auto pr-4">
+      <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+        <div class="h-20 bg-red-400 flex items-center justify-between">
+          <p class="mr-0 text-white text-lg pl-5">TOTAL OPPORTUNITIES</p>
+        </div>
+        <div class="flex-col justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+          <h4>Primary NAICs Here</h4>
+        </div>
+<!-- Return total count of opportunities that match primary NAICs -->
+        <p class="py-4 text-3xl ml-5">41</p>
+        <!-- <hr > -->
       </div>
-      <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
-        <p>TOTAL</p>
+    </div>
+<!--- Second Stats Container --->
+    <div class="container mx-auto pr-4">
+      <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+        <div class="h-20 bg-blue-500 flex items-center justify-between">
+          <p class="mr-0 text-white text-lg pl-5">TEAMING PARTNERS</p>
+        </div>
+        <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+          <p>TOTAL</p>
+        </div>
+        <p class="py-4 text-3xl ml-5">19</p>
+        <!-- <hr > -->
       </div>
-      <p class="py-4 text-3xl ml-5">20,456</p>
-      <!-- <hr > -->
+    </div>
+<!--- Third Stats Container --->
+    <div class="container mx-auto pr-4">
+      <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+        <div class="h-20 bg-purple-400 flex items-center justify-between">
+          <p class="mr-0 text-white text-lg pl-5">MESSAGES</p>
+        </div>
+        <div class="flex justify-between pt-6 px-5 mb-2 text-sm text-gray-600">
+          <p>TOTAL</p>
+        </div>
+        <p class="py-4 text-3xl ml-5">34</p>
+        <!-- <hr > -->
+      </div>
+    </div>
+<!--- Fourth Stats Container --->
+    <div class="container mx-auto">
+      <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+        <div class="h-20 bg-purple-900 flex items-center justify-between">
+          <p class="mr-0 text-white text-lg pl-5">TEAMING REQUESTS</p>
+        </div>
+        <div class="flex justify-between pt-6 px-5 mb-2 text-sm text-gray-600">
+          <p>TOTAL</p>
+        </div>
+        <p class="py-4 text-3xl ml-5">0</p>
+        <!-- <hr > -->
+      </div>
     </div>
   </div>
-      <!---== First Stats Container ====--->
+<!-- FIRST ROW CONTAINING THE  STATS CARD ENDS HERE -->
 
-    <!---== Second Stats Container ====--->
-  <div class="container mx-auto pr-4">
-    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
-      <div class="h-20 bg-blue-500 flex items-center justify-between">
-        <p class="mr-0 text-white text-lg pl-5">BT ACTIVE SUBSCRIBERS</p>
-      </div>
-      <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
-        <p>TOTAL</p>
-      </div>
-      <p class="py-4 text-3xl ml-5">19,694</p>
-      <!-- <hr > -->
+<!-- SECOND ROW CONTAINING THE TEN MOST RECENT CONTRACT OPPORTUNITIES STARTS HERE -->
+  <div class="flex-col justify-center bg-gray-100 py-10 p-5">
+    <h1 class="text-xl mb-4">Ten Most Recent Contract Opportunities</h1>
+    <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
+      <table>
+        <thead>
+          <tr>
+            <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">NAICs Code</th>
+            <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">Title</th>
+            <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">Close Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="hover:bg-grey-lighter">
+            <td class="py-4 px-6 border-b border-grey-light">987098</td>
+            <td class="py-4 px-6 text-center border-b border-grey-light">
+              Title of Contract opportunity here
+            </td>
+            <td class="py-4 px-6 text-center border-b border-grey-light">
+              12/31/2021
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </div>
-    <!---== Second Stats Container ====--->
-
-  <!---== Third Stats Container ====--->
-  <div class="container mx-auto pr-4">
-    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
-      <div class="h-20 bg-purple-400 flex items-center justify-between">
-        <p class="mr-0 text-white text-lg pl-5">BT OPT OUTS</p>
-      </div>
-      <div class="flex justify-between pt-6 px-5 mb-2 text-sm text-gray-600">
-        <p>TOTAL</p>
-      </div>
-      <p class="py-4 text-3xl ml-5">711</p>
-      <!-- <hr > -->
-    </div>
-  </div>
-  <!---== Third Stats Container ====--->
-
-  <!---== Fourth Stats Container ====--->
-  <div class="container mx-auto">
-    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
-      <div class="h-20 bg-purple-900 flex items-center justify-between">
-        <p class="mr-0 text-white text-lg pl-5">BT TODAY'S SUBSCRIPTION</p>
-      </div>
-      <div class="flex justify-between pt-6 px-5 mb-2 text-sm text-gray-600">
-        <p>TOTAL</p>
-      </div>
-      <p class="py-4 text-3xl ml-5">0</p>
-      <!-- <hr > -->
-    </div>
-  </div>
-  <!---== Fourth Stats Container ====--->
-</div>
-<!---===================== FIRST ROW CONTAINING THE  STATS CARD ENDS HERE =============================-->
-
-<!------===================== SECOND ROW CONTAINING THE TABLE STATS STARTS HERE =============================-->
-<div class="flex justify-center bg-gray-100 py-10 p-5">
-  <!--==== frist div begins here ====--->
-  <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
+<!-- first div begins here  -->
+  <!-- <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
     <div class="w-11/12 mx-auto">
       <div class="bg-white my-6">
-        <table class="text-left w-full border-collapse"> <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
+        <table class="text-left w-full border-collapse"> 
           <thead>
             <tr>
               <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">KEYWORDS</th>
@@ -109,15 +126,15 @@
           </tbody>
         </table>
       </div>
-      </div>
-        </div>
-    <!--==== frist div ends here ====--->
+    </div>
+  </div> -->
+<!-- first div ends here --->
 
-    <!--==== Second div begins here ====--->
-    <div class="container mr-5 mx-auto bg-white shadow-xl">
+<!-- Second div begins here --->
+    <!-- <div class="container mr-5 mx-auto bg-white shadow-xl">
       <div class="w-11/12 mx-auto">
         <div class="bg-white my-6">
-          <table class="text-left w-full border-collapse"> <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
+          <table class="text-left w-full border-collapse">
             <thead>
               <tr>
                 <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">MSISDN</th>
@@ -159,14 +176,14 @@
           </table>
         </div>
         </div>
-          </div>
-      <!--==== Second div ends here ====--->
+          </div> -->
+<!-- Second div ends here --->
 
-      <!--==== Third div begins here ====--->
-      <div class="container mx-auto bg-white shadow-xl">
+<!-- Third div begins here --->
+      <!-- <div class="container mx-auto bg-white shadow-xl">
         <div class="w-11/12 mx-auto">
           <div class="bg-white my-6">
-            <table class="text-left w-full border-collapse"> <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
+            <table class="text-left w-full border-collapse">
               <thead>
                 <tr>
                   <th class="py-4 px-6 bg-purple-400 font-bold uppercase text-sm text-white border-b border-grey-light">MSISDN</th>
@@ -207,8 +224,8 @@
               </tbody>
             </table>
           </div>
-          </div>
-            </div>
-        <!--==== Third div ends here ====--->
-    </div>
-<!---------===================== SECOND ROW CONTAINING THE TABLE STATS ENDS HERE =============================-->
+        </div>
+      </div> -->
+<!-- Third div ends here --->
+  </div>
+<!-- SECOND ROW CONTAINING THE TABLE STATS ENDS HERE -->
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -6,7 +6,8 @@
 <!-- main content -->
 <main class="flex-grow flex flex-col min-h-0 border-t"> 
   <!-- content navigation -->
-  <DashboardStatistics />
+  <!-- <DashboardStatistics /> -->
+
   <!-- section content -->
   <section aria-label="main content" class="flex min-h-0 flex-col flex-auto border-l">
     <Charts />
