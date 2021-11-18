9e37585928c049c896ca2c2a77c12a4f63aea281
Jason Gutierrez <jasong84@gmail.com>
2021-11-18 00:41:33 -0600

    Restructure dynamic solicitation component

--- src/routes/solicitations/[id].svelte
+++ src/routes/solicitations/[id].svelte
@@ -6,37 +6,24 @@
         };
     }
 </script>
+
 <script>
-    import { get, post } from '$lib/utils';
+    import { get } from '$lib/utils';
     import SolicitationCard from '$lib/components/solicitations/solicitationCard.svelte'
     import PointOfContact from '$lib/components/solicitations/pointOfContact.svelte';
-    import ActionCard from '$lib/components/solicitations/actionCard.svelte'
-    import { auth } from '$lib/shared/user-store';
-   
-    let didSaveSolicitation;
+
     export let solicitationId;
     
-
     async function getSolicitation() {
         const response = await get(`solicitations/${ solicitationId }`)
         if(response.ok) return response.json();
-    }
-
-    async function saveSolicitation(e) {
-        const saveSolicitationResponse = await post("entities/saveSolicitation", {
-            solicitation: solicitationId,
-            entityId: $auth.user.entity.id
-        })
-        if(saveSolicitationResponse.ok){
-            didSaveSolicitation = true;
-        }
-    }
+    }  
 
 </script>
 
 {#await getSolicitation() then solicitation}
-    <SolicitationCard contract={ solicitation } />
-    <PointOfContact contract={ solicitation } />
-    <ActionCard on:addsolicitation={saveSolicitation} bind:solicitationSelected={didSaveSolicitation} />
-   
+    <div class="flex flex-col md:flex-row">
+        <SolicitationCard contract={ solicitation } />
+        <PointOfContact contract={ solicitation } />
+    </div>
 {/await}
