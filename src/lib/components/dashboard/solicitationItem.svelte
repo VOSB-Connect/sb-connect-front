7b2ef249e0788e57e20a5fdea092ea29677ee920
Jason Gutierrez <jasong84@gmail.com>
2021-11-17 23:30:38 -0600

    Restructure dashboard / solications page

---
+++ src/lib/components/dashboard/solicitationItem.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let contract;
+    console.log(contract.published_at)
+</script>
+
+<a href={`/solicitations/${contract.id}`} class="hover:bg-blue-100 bg-gray-100 flex w-full gap-x-4 px-3 py-2 items-center justify-center cursor-pointer">
+    <p>{contract.naicsCode}</p>
+    <p class="flex-1 w-24">{contract.title}</p>
+    <p>{contract.published_at}</p>
+</a>
