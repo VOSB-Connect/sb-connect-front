481fd343eefc387a8f6dc54867a77829f3559d42
Jason Gutierrez <jasong84@gmail.com>
2021-11-18 00:40:40 -0600

    Adjust layout of button to save solicitations

--- src/lib/components/solicitations/actionCard.svelte
+++ src/lib/components/solicitations/actionCard.svelte
@@ -4,7 +4,8 @@
     export let solicitationSelected;
 </script>
 
-<button class="{solicitationSelected ? "bg-green-500": "bg-blue-500"} block w-full text-white text-sm font-semibold rounded-lg  py-1 px-2"
-on:click={() => dispatch('addsolicitation')}
->{solicitationSelected ? "Saved Solicitation" : "Save Solicitation"}
+<button 
+    class="{solicitationSelected ? "bg-green-500": "bg-blue-500"} w-full md:w-1/4 text-white text-sm font-semibold rounded-lg h-12 py-1 px-2"
+    on:click={() => dispatch('addsolicitation')}>
+    {solicitationSelected ? "Saved Solicitation" : "Save Solicitation"}
 </button>  
