b9c3839b892fd8eeabf5df08844e915caa65f73c
Jason Gutierrez <jasong84@gmail.com>
2021-11-23 16:44:14 -0600

    Update export variable to solicitation from contract

---
+++ src/routes/dashboard/solicitations/[id].svelte
@@ -0,0 +1,30 @@
+<script context="module">
+    export async function load({ page }) {
+        const solicitationId = page.params.id;
+        return {
+            props: { solicitationId }
+        };
+    }
+</script>
+
+<script>
+    import { get } from '$lib/utils';
+    import SolicitationCard from '$lib/components/solicitations/solicitationCard.svelte'
+    import PointOfContact from '$lib/components/solicitations/pointOfContact.svelte';
+
+    export let solicitationId;
+    $:getSolicitation().then(console.log)
+    async function getSolicitation() {
+        const response = await get(`solicitations/${ solicitationId }`)
+        if(response.ok) return response.json();
+    }  
+
+</script>
+
+
+{#await getSolicitation() then solicitation}
+    <div class="flex flex-col lg:flex-row">
+        <SolicitationCard contract={ solicitation } />
+        <PointOfContact contract={ solicitation } />
+    </div>
+{/await}
