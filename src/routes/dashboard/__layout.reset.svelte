44e285826389c57c529a997b5276233bac15581d
Jason Gutierrez <jasong84@gmail.com>
2022-01-08 17:55:43 -0600

    Update dashboard layout to fix responsive design issue.

--- src/routes/dashboard/__layout.reset.svelte
+++ src/routes/dashboard/__layout.reset.svelte
@@ -7,12 +7,12 @@
   <title>CAPTR Labs</title>
 </svelte:head>
 <ProtectedLayout>
-    <div class="h-screen w-full flex flex-col md:flex-row overflow-auto antialiased text-gray-800 bg-gray-100">
+    <div class="h-screen w-full flex flex-col lg:flex-row overflow-auto antialiased text-gray-800 bg-gray-100">
         <!-- section body side nav -->
         <Sidebar />
 
         <SidebarMobile />
-        <div class="flex-1 flex flex-col md:flex-row justify-start items-start md:px-2 pt-2 md:pt-5 md:pr-5">
+        <div class="flex-1 flex flex-col md:flex-row justify-start items-start lg:px-2 pt-2 lg:pt-5 lg:pr-5">
             <slot />
         </div>
     </div>
