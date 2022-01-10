25aa04c1a0ac422fad839c378bb4099eaf6dacc8
Unknown <blossomperez18@gmail.com>
2022-01-10 17:50:36 -0600

    Create MobileNav component
    -Changed bg color to mobile menu
    -Added animation
    -Created separate component
    -Fixed navbar to top

---
+++ src/lib/components/mobilenav.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { auth } from '$lib/shared/user-store'
+    import { faTimes } from '@fortawesome/free-solid-svg-icons'
+    import Fa from 'svelte-fa'
+    
+    export let open = false;
+</script>
+
+  <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+  <aside class="md:hidden bg-purple-900 w-60 fixed z-50 h-full" class:open id="mobile-menu">
+    <div class="pt-10 pr-6 pl-6 space-y-1">
+        <a href="#"  on:click="{() => open = !open}">
+            <Fa icon={faTimes} size="lg" class="ml-auto cursor-pointer" />
+        </a>
+          <!-- <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
+        href="/">Home</a> -->
+        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="https://calendly.com/atknho/acceleratorcapturemvp">CAPTR Labs</a>
+        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/contact">Autobot Labs</a>
+        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/contact">Teaming Labs</a>
+        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/pricing">Pricing</a>
+        {#if $auth !== null && $auth.jwt}
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+                href="dashboard">Dashboard</a>
+        {:else}
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400" href="account/login">Login</a>
+            <a class="px-4 py-1 text-sm font-medium text-center text-gray-200 transition-colors duration-300 transform border rounded hover:bg-indigo-400"
+                href="account/registration">Register</a>
+        {/if}
+    </div>
+</aside>
+
+
+<style>
+    aside {
+		left: -100%;
+		transition: left 0.3s ease-in-out
+	}
+	
+	.open {
+		left: 0
+	}
+</style>
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,14 +1,15 @@
 <script>
     import { auth } from '$lib/shared/user-store'
+    import Mobilenav from './mobilenav.svelte';
 
-    let close = true;
+    let open = false;
 </script>
 
-<nav class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+<nav class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center fixed">
     <div class="flex items-center justify-between">
          <!-- Mobile menu button -->
          <div class="flex md:hidden">
-            <button type="button" on:click="{() => close = !close}" class="text-grey-200 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+            <button type="button" on:click="{() => open = !open}" class="text-grey-200 hover:text-gray-400 focus:outline-none focus:text-gray-400"
             aria-controls="mobile-menu" aria-expanded="false">
                 <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
                     <path fill-rule="evenodd"
@@ -20,30 +21,6 @@
 
         <a class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-indigo-400"
             href="/">CAPTR</a>
-    </div>
-
-    <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-    <div class="md:hidden absolute" class:close id="mobile-menu">
-        <div class="px-2 pt-4 pb-3 space-y-1">
-              <!-- <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="/">Home</a> -->
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
-                href="https://calendly.com/atknho/acceleratorcapturemvp">CAPTR Labs</a>
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
-                href="/contact">Autobot Labs</a>
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
-                href="/contact">Teaming Labs</a>
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
-                href="/pricing">Pricing</a>
-            {#if $auth !== null && $auth.jwt}
-                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
-                    href="dashboard">Dashboard</a>
-            {:else}
-                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400" href="account/login">Login</a>
-                <a class="px-4 py-1 text-sm font-medium text-center text-gray-200 transition-colors duration-300 transform border rounded hover:bg-indigo-400"
-                    href="account/registration">Register</a>
-            {/if}
-        </div>
     </div>
 
     <!-- Desktop Menu -->
@@ -69,9 +46,4 @@
     </div>
 </nav>
 
-
-<style>
-    .close {
-        display: none;
-    }
-</style>
+<Mobilenav bind:open />
