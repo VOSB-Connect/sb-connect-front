4d1ede989209705479fe7f13ec224e8e02f4237a
Jason Gutierrez <jasong84@gmail.com>
2022-01-11 14:40:09 -0600

    Modify navbar component and make navbarMobile component

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,48 +1,44 @@
 <script>
     import { auth } from '$lib/shared/user-store'
-    import Mobilenav from './mobilenav.svelte';
+    import NavbarMobile from '$lib/components/navbarMobile.svelte';
 
-    let open = false;
+    let displayMenu = false;
 </script>
 
-<nav class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center fixed">
-    <div class="m-auto flex items-center justify-between">
-         <!-- Mobile menu button -->
-         <div class="flex md:hidden">
-            <button type="button" on:click="{() => open = !open}" class="text-grey-200 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-            aria-controls="mobile-menu" aria-expanded="false">
-                <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                    <path fill-rule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
-                    </path>
-                </svg>
+<nav class="w-full px-6 py-4 md:flex md:justify-between md:items-center fixed bg-indigo-500">
+    <div class="w-full xl:w-9/12 lg:w-10/12 mx-auto flex justify-between items-center">
+        <!-- Mobile menu button -->
+        <div class="block md:hidden">
+            <button type="button" on:click|preventDefault="{() => displayMenu = !displayMenu}" class="z-10 text-white w-10 h-10 flex flex-col items-center justify-center gap-y-1" aria-controls="mobile-menu" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <span aria-hidden="true" class="block h-0.5 w-5 bg-current"></span>
+                <span aria-hidden="true" class="block h-0.5 w-5 bg-current"></span>
+                <span aria-hidden="true" class="block h-0.5 w-5 bg-current"></span>
             </button>
         </div>
-
-        <a class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-indigo-400"
-            href="/">CAPTR</a>
-    </div>
-
-    <!-- Desktop Menu -->
-    <div class="flex-col mt-2 space-y-4 hidden md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
-        <!-- <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="/">Home</a> -->
-        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="https://calendly.com/atknho/acceleratorcapturemvp">CAPTR Labs</a>
-        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="/contact">Autobot Labs</a>
-        <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="/contact">Teaming Labs</a>
+        <a class="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-indigo-400" href="/">CAPTR</a>
+        
+        <!-- Desktop Menu -->
+        <div class="flex-row mt-2 space-y-4 hidden md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+            <!-- <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
+                href="/">Home</a> -->
             <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="/pricing">Pricing</a>
-        {#if $auth !== null && $auth.jwt}
+                href="https://calendly.com/atknho/acceleratorcapturemvp">CAPTR Labs</a>
             <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
-            href="dashboard">Dashboard</a>
-        {:else}
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400" href="account/login">Login</a>
-            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400" href="account/registration">Register</a>
-        {/if}
+                href="/contact">Autobot Labs</a>
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
+                href="/contact">Teaming Labs</a>
+                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
+                href="/pricing">Pricing</a>
+            {#if $auth !== null && $auth.jwt}
+                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400"
+                href="dashboard">Dashboard</a>
+            {:else}
+                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400" href="account/login">Login</a>
+                <a class="text-sm font-medium text-gray-200 transition-colors duration-300 transform hover:text-indigo-400" href="account/registration">Register</a>
+            {/if}
+        </div>
     </div>
 </nav>
 
-<Mobilenav bind:open />
+<NavbarMobile bind:displayMenu />
---
+++ src/lib/components/navbarMobile.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { auth } from '$lib/shared/user-store'
+    import { faTimes } from '@fortawesome/free-solid-svg-icons'
+    import Fa from 'svelte-fa'
+    
+    export let displayMenu;
+</script>
+
+  <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+  <section class="md:hidden bg-purple-900 w-full fixed z-50 {displayMenu ? "translate-x-0" : "-translate-x-full" } ease-in-out duration-500">
+    <div class="py-4 px-6">
+        <button type="button" class="ml-auto absolute right-10 top-6 text-white"  on:click="{() => displayMenu = !displayMenu}">
+            <Fa icon={faTimes} size="lg" />
+        </button>
+		<div class="flex flex-col w-max">
+			<a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block w-max transform hover:text-indigo-400"
+            href="https://calendly.com/atknho/acceleratorcapturemvp">CAPTR Labs</a>
+			<a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/contact">Autobot Labs</a>
+			<a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/contact">Teaming Labs</a>
+			<a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+            href="/pricing">Pricing</a>
+			{#if $auth !== null && $auth.jwt}
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400"
+			href="dashboard">Dashboard</a>
+			{:else}
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400" href="account/login">Login</a>
+            <a class="text-sm font-medium text-gray-200 transition-colors duration-300 px-3 py-2 block transform hover:text-indigo-400" href="account/registration">Register</a>
+			{/if}
+		</div>
+    </div>
+</section>
