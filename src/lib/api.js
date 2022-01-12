3bac112f2a07fe22a421abf2a5d6614c3e24f59e
Jason Gutierrez <jasong84@gmail.com>
2022-01-11 21:30:48 -0600

    Edit api utility

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,5 @@
 // const isProduction = false;
-import { dev } from '$app/env'
-export const strapiBase = (dev) ? "http://localhost:1337" : "https://sb-connect-back-strapi.herokuapp.com"
+
+export const strapiBase = import.meta.env.VITE_NODE_ENV == "dev"
+	? 'http://localhost:1337'
+	: 'https://sb-connect-back-strapi.herokuapp.com/';
