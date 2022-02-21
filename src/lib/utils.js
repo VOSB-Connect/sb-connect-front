49ea26ac1b47553f30ef6b816906f860add5df12
Unknown <blossomperez18@gmail.com>
2022-02-20 18:54:04 -0600

    Create public calls

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,7 +5,7 @@ export async function post(endpoint, body){
 
    try{
     let headers = {}
-    headers['Content-Type'] = 'application/json',
+    headers['Content-Type'] = 'application/json'
     body = JSON.stringify(body || {})
     
     const token = window.sessionStorage.getItem("auth");
@@ -36,6 +36,37 @@ export async function post(endpoint, body){
    }
 }
 
+export async function publicPost(endpoint, body){
+    let customError = false
+ 
+    try{
+     let headers = {}
+     headers['Content-Type'] = 'application/json'
+     body = JSON.stringify(body || {})
+      
+     
+     const response = await fetch(`${strapiBase}/${endpoint}`, { method: 'POST', body, headers})
+     if(!response.ok){
+         try {
+             const data = await response.json();
+             return data;
+         } catch(err){
+             console.error(err);
+         }    
+     }
+     try { 
+         return response;
+     } catch(err){
+         console.error(err);
+         throw err;
+     }
+    }catch(err){
+     console.error(err);
+     throw customError ? err : {id: '', message: 'An unknown error occured'}
+    }
+ }
+ 
+
 export async function get(endpoint) {
     let customError = false;
     try {
@@ -43,10 +74,37 @@ export async function get(endpoint) {
         headers['Content-Type'] = 'application/json';
 
         const token = window.sessionStorage.getItem("auth");
-        if (token !== '') {
+        if (Object.keys(token).length !== 0) {
             const authToken = JSON.parse(token).jwt;
             headers['Authorization'] = `Bearer ${authToken}`;
         } 
+        const response = await fetch(`${strapiBase}/${endpoint}`, { method: 'GET', headers });
+        if(!response.ok){
+            try {
+                const data = await response.json();
+                return data;
+            } catch (err) {
+                console.error(err);
+            } 
+        }
+        try {
+            return response;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    } catch (error) {
+        console.error(error);
+		throw customError ? error : { id: '', message: 'An unknown error occured' };
+    }
+}
+
+export async function getPublic(endpoint) {
+    let customError = false;
+    try {
+        let headers = {}
+        headers['Content-Type'] = 'application/json';
+        
         const response = await fetch(`${strapiBase}/${endpoint}`, { method: 'GET', headers });
         if(!response.ok){
             try {
