53ed47d3ebb8c909630ac5ad8cdb4e2c1b19d8c4
Unknown <blossomperez18@gmail.com>
2022-03-08 16:27:25 -0600

    Update my solicitations page

--- src/lib/components/solicitations/savedSolicitation.svelte
+++ src/lib/components/solicitations/savedSolicitation.svelte
@@ -1,11 +1,15 @@
 <script>
     import { formatDate } from '$lib/utils'
-    import SolicitationAttachments from './solicitationAttachments.svelte';
+    // import SolicitationAttachments from './solicitationAttachments.svelte';
+    import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons'
+    import Fa from 'svelte-fa'
     export let solicitation;
-    let title = solicitation.title
-    let solicitaitonNo = solicitation.solicitationNumber
-    let postDate = formatDate(solicitation.published_at)
-    let solicitationStatus = solicitation.active === "Yes" ? "Active" : "Inactive";
+
+    const { title, solicitationNumber, published_at, active, naicsCode, uiLink } = solicitation
+
+
+    let postDate = formatDate(published_at)
+    let solicitationStatus = active === "Yes" ? "Active" : "Inactive";
     let statusColors = solicitationStatus === "Active" ? {color: "text-green-700", bg: "bg-green-100", border: "border-green-300"} : {color: "text-red-700", bg: "bg-red-100", border: "border-red-300"};
     
     
@@ -27,16 +31,16 @@
 <!-- Card -->
 <div class="w-full m-2 md:w-1/4 justify-center items-center bg-white shadow-lg rounded-lg flex flex-col">
     <div class="w-full p-4 justify-center flex flex-col">
-        <p class="border-b-2 text-xl">{title}</p>
-        <p class="my-0 text-gray-500"><span class="font-bold">Solicitation No:</span> { solicitaitonNo }</p>
+        <p class="border-b-2 text-xl flex justify-between items-center w-100"><span>{ title }</span> <a href={ uiLink } target="_blank"><Fa icon={ faExternalLinkSquareAlt } size="md" /></a></p>
+        <p class="my-0 text-gray-500"><span class="font-bold">Solicitation No:</span> { solicitationNumber }</p>
         <p class="my-0 text-gray-500"><span class="font-bold">Posted Date:</span> { postDate }</p>  
-        <p class="my-0 text-gray-500"><span class="font-bold">NAICs Code:</span> { solicitation.naicsCode }</p>
-        {#if displaySolicationAddress(solicitation.placeOfPerformance) !== ""}
+        <p class="my-0 text-gray-500"><span class="font-bold">NAICs Code:</span> { naicsCode }</p>
+        <!-- {#if displaySolicationAddress(solicitation.placeOfPerformance) !== ""}
             <p class="my-0 text-gray-500"><span class="font-bold">Place of Performance:</span> { displaySolicationAddress(solicitation.placeOfPerformance) }</p>   
-        {/if}  
+        {/if}   -->
         <div class="flex w-1/4 justify-center items-center m-1 font-medium py-1 px-2 rounded-full border {statusColors.bg} {statusColors.color} {statusColors.border}">
             <div class="text-xs font-normal leading-none max-w-full flex-initial">{solicitationStatus}</div>
         </div>
-        <SolicitationAttachments {solicitation} />
+        <!-- <SolicitationAttachments {solicitation} /> -->
     </div>
 </div>
