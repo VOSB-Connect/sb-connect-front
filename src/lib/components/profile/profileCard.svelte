903464b8715e6cbc75f2daea689616eec6822de9
Unknown <blossomperez18@gmail.com>
2022-03-09 19:18:49 -0600

    Add company info from organization

--- src/lib/components/profile/profileCard.svelte
+++ src/lib/components/profile/profileCard.svelte
@@ -2,12 +2,13 @@
 	import Icon from '$lib/components/dashboard/dashboardIcons.svelte'
 	import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
 	export let business;
+
+	const { primaryNaics, purposeOfRegistrationDesc, updated_at, legalBusinessName, registrationStatus} = business
 	
-	let primaryNaics = business.assertions.goodsAndServices.PrimaryNaics ?? "n/a";
-	let purposeOfRegistrationDesc = business.entityRegistration.purposeOfRegistrationDesc;
+	let naics = primaryNaics ?? "n/a";
 
 	const oneDay = (1 * 24 * 60 * 60 * 1000);
-	const currentTimeSpan = Date.now() - new Date(business.lastProfileUpdate).getTime();
+	const currentTimeSpan = Date.now() - new Date(updated_at).getTime();
 	const canUpdate = currentTimeSpan > oneDay;
 
 	function handleProfileUpdate(){
@@ -17,29 +18,29 @@
 
 <div class="bg-white shadow-sm p-3 border-t-4 row-span-3 col-span-1 border-green-400">
 	<div class="flex justify-between items-center">
-		<h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{business.entityRegistration.legalBusinessName}</h1>
+		<h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{ legalBusinessName }</h1>
 		{#if canUpdate}
-			<button type="button" class="bg-green-500 py-1 px-2 rounded text-white text-sm" on:click={handleProfileUpdate} >Update Profile</button>
+			<button type="button" class="bg-green-500 py-1 px-2 rounded text-white text-sm" on:click={ handleProfileUpdate } >Update Profile</button>
 		{/if}
 		
 	</div>
 	<ul class="bg-gray-100 text-gray-600 py-2 px-3 my-3 divide-y rounded shadow-sm">
 		<li class="flex items-center justify-between py-3">
 			<span>SAM Status</span>
-			<span class="bg-green-500 py-1 px-2 rounded text-white text-sm">Active</span>
+			<span class="bg-green-500 py-1 px-2 rounded text-white text-sm">{ registrationStatus }</span>
 		</li>
 		<li class="flex items-center justify-between py-3">
 			<span>Purpose of Registration</span>
-			<span class="py-1 px-2 text-sm">{purposeOfRegistrationDesc}</span>
+			<span class="py-1 px-2 text-sm">{ purposeOfRegistrationDesc }</span>
 		</li>
 		<li class="flex items-center justify-between py-3">
 			<span>Primary NAICs</span>
-			<span class="ml-auto">{primaryNaics}</span>
+			<span class="ml-auto">{ naics }</span>
 		</li>
 		<li class="flex items-center justify-between py-3">
 			<span>Statement of Capability</span>
 			<a href="/">
-				<Icon iconName={faFilePdf} />
+				<Icon iconName={ faFilePdf } />
 			</a>
 		</li>
 	</ul>
