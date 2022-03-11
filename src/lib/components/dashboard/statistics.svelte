068165e448a7e8feb19a2a2c0faed0b5fd7a8e71
Unknown <blossomperez18@gmail.com>
2022-03-10 21:30:17 -0600

    Change color of buttons to match the  corresponding pills in table

--- src/lib/components/dashboard/statistics.svelte
+++ src/lib/components/dashboard/statistics.svelte
@@ -25,7 +25,8 @@
     <p id="ticket-statistics-tabs-label" class="font-semibold block mb-1 text-sm">Daily Solicitations Breakdown</p>
     <ul class="flex">
       <li>
-        <button class="focus:outline-none focus:bg-yellow-200 p-2 rounded-l-md border border-r-0 bg-white flex flex-col items-center w-24"  >
+        <button 
+        class="bg-white focus:outline-none focus:bg-yellow-200 p-2 rounded-l-md border border-r-0  flex flex-col items-center w-24"  >
           <p class="font-semibold text-lg">{ count.total }</p>
           <p class="uppercase text-gray-600 text-sm">
             Total
@@ -33,34 +34,34 @@
         </button>
       </li>
       <li>
-        <button class="focus:outline-none focus:bg-yellow-200 p-2 border border-r-0 bg-white flex flex-col items-center w-24"  >
-          <p class="font-semibold text-lg">{ count["Solicitation"][0] }</p>
-          <p class="uppercase text-gray-600 text-sm">
+        <button class="bg-green-500 focus:outline-none focus:bg-yellow-200 p-2 border border-r-0  flex flex-col items-center w-24"  >
+          <p class="font-semibold text-white text-lg">{ count["Solicitation"][0] }</p>
+          <p class="uppercase text-white text-sm">
             Sol
           </p>
         </button>
       </li>
       <li>
-        <button class="focus:outline-none focus:bg-yellow-200 p-2 border border-r-0 bg-white flex flex-col items-center w-24" >
-          <p class="font-semibold text-lg">{ count["Presolicitation"][0] }</p>
-          <p class="uppercase text-gray-600 text-sm">
+        <button class="bg-red-600 focus:outline-none focus:bg-yellow-200 p-2 border border-r-0 flex flex-col items-center w-24" >
+          <p class="font-semibold text-white text-lg">{ count["Presolicitation"][0] }</p>
+          <p class="uppercase text-white text-sm">
             Pre
           </p>
         </button>
       </li>
       <li>
-        <button class="focus:outline-none focus:bg-yellow-200 p-2 border border-r-0 bg-white flex flex-col items-center w-24" >
-          <p class="font-semibold text-lg">{ count["Combined Synopsis/Solicitation"][0] }</p>
-          <p class="uppercase text-gray-600 text-sm">
+        <button class="bg-purple-600 focus:outline-none focus:bg-yellow-200 p-2 border border-r-0  flex flex-col items-center w-24" >
+          <p class="font-semibold text-white text-lg">{ count["Combined Synopsis/Solicitation"][0] }</p>
+          <p class="uppercase text-white text-sm">
             Combined
           </p>
         </button>
       </li>
 
       <li>
-        <button class="focus:outline-none focus:bg-yellow-200 p-2 border border-r-0 bg-white flex flex-col items-center w-24" >
-          <p class="font-semibold text-lg">{ count["Sources Sought"][0] }</p>
-          <p class="uppercase text-gray-600 text-sm">
+        <button class="bg-yellow-500 focus:outline-none focus:bg-yellow-200 p-2 rounded-r-md border flex flex-col items-center w-24" >
+          <p class="font-semibold text-white text-lg">{ count["Sources Sought"][0] }</p>
+          <p class="uppercase text-white text-sm">
             Sources
           </p>
         </button>
