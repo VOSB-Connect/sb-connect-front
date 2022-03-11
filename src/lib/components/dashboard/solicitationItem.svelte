b29a245648ad6bdd1179f96f5682ac8816b322ed
Unknown <blossomperez18@gmail.com>
2022-03-10 21:10:00 -0600

    Update expiration date to only show date w/o time

--- src/lib/components/dashboard/solicitationItem.svelte
+++ src/lib/components/dashboard/solicitationItem.svelte
@@ -3,13 +3,14 @@
 
     export let contract;
     export let count = {};
-    const { id, naicsCode, title, published_at, baseType } = contract;
+    const { id, naicsCode, title, baseType, archiveDate } = contract;
+    let [date, time] = formatDate(archiveDate).split(",")
 
 </script>
 
-<a href={`/solicitations/${ id }`} class="hover:bg-blue-100 bg-gray-100 flex w-full gap-x-4 px-3 py-2 items-center justify-center cursor-pointer">
+<a href={ `/solicitations/${ id }` } class="hover:bg-blue-100 bg-gray-100 flex w-full gap-x-4 px-3 py-2 items-center justify-center cursor-pointer">
     <p>{ naicsCode }</p>
-    <p class="flex-1 w-24">{ title }</p>
-    <p class="px-6 py-2 { count[baseType][1] } text-white font-medium text-xs uppercase rounded-full shadow-md  focus:bg-blue-700 focus:shadow-lg  focus:ring-0">{ baseType }</p>
-    <p>{ formatDate(published_at) }</p>
+    <p>{ title }</p>
+    <p class="px-6 py-2  { count[baseType][1] } text-white font-medium text-xs uppercase rounded-full shadow-md  focus:bg-blue-700 focus:shadow-lg  focus:ring-0">{ baseType }</p>
+    <p class="ml-auto">{ date }</p>
 </a>
