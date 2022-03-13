913b2042def7d34f81db08455c91c54b82057635
Unknown <blossomperez18@gmail.com>
2022-03-13 00:27:12 -0500

    Flex wrap buttons of sol numbers on mobile view

--- src/lib/components/dashboard/statistics.svelte
+++ src/lib/components/dashboard/statistics.svelte
@@ -22,8 +22,8 @@
 <nav class="bg-gray-100 flex p-4">
   <!-- stats nav -->
   <section aria-labelledby="ticket-statistics-tabs-label" class="pb-2">
-    <p id="ticket-statistics-tabs-label" class="font-semibold block mb-1 text-sm">Daily Solicitations Breakdown</p>
-    <ul class="flex">
+    <p id="ticket-statistics-tabs-label" class="font-semibold block mb-1 text-sm text-center md:text-left">Daily Solicitations Breakdown</p>
+    <ul class="flex flex-wrap justify-center">
       <li>
         <button 
         class="bg-white focus:outline-none focus:bg-yellow-200 p-2 rounded-l-md border border-r-0  flex flex-col items-center w-24"  >
