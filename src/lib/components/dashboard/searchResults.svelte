028e302d568180af78e56b2374366510f5252e2d
Jason Gutierrez <jasong84@gmail.com>
2022-03-13 17:41:43 -0500

    Extend the height of solicitations container

--- src/lib/components/dashboard/searchResults.svelte
+++ src/lib/components/dashboard/searchResults.svelte
@@ -7,12 +7,18 @@
 </script>
  
 
-<section class="flex w-full flex-col flex-1 divide-y min-h-0 max-h-96 {$SolicitationStore.length == 0 ? "" : "overflow-auto"} px-0">
+<section class="flex w-full flex-col flex-1 divide-y min-h-0 {$SolicitationStore.length == 0 ? "" : "overflow-auto"} px-0">
 	{#if $SolicitationStore.length > 0}
 		{#each $SolicitationStore as solicitation}
 			<SolicitationItem contract={ solicitation } count={ count } />
 		{/each}
-		{:else}
+	{:else}
 			<p>No Solicitations</p>
 	{/if}
 </section>
+
+<style>
+	section {
+		height: calc(100vh - 250px);
+	}
+</style>
