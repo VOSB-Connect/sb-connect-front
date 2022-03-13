f1758c0e6e6ec04c6feb3aa5a03774a6d88eadfb
Unknown <blossomperez18@gmail.com>
2022-03-12 23:42:43 -0500

    Fix alignment of listed solicitation div

--- src/lib/components/dashboard/searchResults.svelte
+++ src/lib/components/dashboard/searchResults.svelte
@@ -7,7 +7,7 @@
 </script>
  
 
-<section class="flex w-full flex-col flex-1 divide-y min-h-0 results-container {$SolicitationStore.length == 0 ? "" : "overflow-auto"} px-0">
+<section class="flex w-full flex-col flex-1 divide-y min-h-0 max-h-96 {$SolicitationStore.length == 0 ? "" : "overflow-auto"} px-0">
 	{#if $SolicitationStore.length > 0}
 		{#each $SolicitationStore as solicitation}
 			<SolicitationItem contract={ solicitation } count={ count } />
@@ -16,9 +16,3 @@
 			<p>No Solicitations</p>
 	{/if}
 </section>
-
-<style>
-	.results-container{
-		height: calc(100vh - 250px);
-	}
-</style>
