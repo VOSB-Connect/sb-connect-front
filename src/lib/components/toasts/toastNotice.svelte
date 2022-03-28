4fcbbdcad80f2d5b1741f488fc3c9ca43dfb1c31
Cpt_Crunch <jasong84@gmail.com>
2022-03-27 20:02:50 -0500

    Update Toast Notice component when saving/removing solicitation

--- src/lib/components/toasts/toastNotice.svelte
+++ src/lib/components/toasts/toastNotice.svelte
@@ -4,7 +4,8 @@
 	import { fly } from 'svelte/transition'
 	import { quintOut } from 'svelte/easing'
 
-	export let showToastNotice;
+	export let showNotice;
+	export let noticeClass = "bg-green-600";
 	const options = {
 		delay: 250,
 		duration: 600,
@@ -14,22 +15,27 @@
 	}
 
 	$: {
-		if (showToastNotice){
-			setTimeout(() => showToastNotice = false, 4000)
+		if (showNotice){
+			setTimeout(() => showNotice = false, 3000)
 		}
 	}
+
+
 </script>
 
 
-{#if showToastNotice}
+{#if showNotice}
 	<div class="fixed z-10 top-1 right-1" transition:fly={options}>	
 		<div class="flex max-w-sm w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto">
-			<div class="w-2 bg-green-600"></div>
+			<div class="w-2 {noticeClass}"></div>
 			<div class="w-full flex justify-between items-start px-2 py-2">
 				<div class="flex flex-col ml-2">
-					<p class="text-gray-800">You successfully saved this Solicitation!</p>
+					<p class="text-gray-800">
+						<slot />
+						
+					</p>
 				</div>
-				<button type="button" on:click={() => showToastNotice = false} class="pl-2 text-gray-500">
+				<button type="button" on:click={() => showNotice = false} class="pl-2 text-gray-500">
 					<Fa icon={faTimes} size="lg" />
 				</button>
 			</div>
