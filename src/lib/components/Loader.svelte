4048267f3fd0cc6acebad4826556675f378a9406
Cpt_Crunch <jasong84@gmail.com>
2022-04-02 12:54:28 -0500

    Create Loader component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { isOverlayOpen } from '$lib/stores/OverlayStore.js';
+</script>
+
+{#if $isOverlayOpen }
+	<div class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center opacity-90 z-20">
+		<div class="bg-gray-900 text-white rounded-md p-48 relative max-w-2xlflex items-center justify-center opacity-95">
+			<div class="w-12 h-12 rounded-full animate-spin border-8 border-solid border-purple-500 border-t-transparent"></div>
+		</div>
+	</div>
+{/if}
+	
