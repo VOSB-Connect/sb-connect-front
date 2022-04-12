4eb298a12b3074eb604b7a32cdb94167c7b81d17
Unknown <blossomperez18@gmail.com>
2022-04-12 13:59:52 -0500

    Add error message if passwords do not match

--- src/routes/account/reset-password.svelte
+++ src/routes/account/reset-password.svelte
@@ -10,8 +10,9 @@
 </script>
 
 <script>
-    import { post } from '$lib/utils'
-    import { goto } from '$app/navigation'
+    import { post } from '$lib/utils';
+    import { goto } from '$app/navigation';
+    import ListError from '$lib/ListError.svelte';
 
     export let url;
 
@@ -20,10 +21,12 @@
 
     let password = "";
     let passwordConfirmation = "";
-    console.log(code)
+    let error = null;
+
     async function handleResetPassword() {
         if(password !== passwordConfirmation) {
-            return 'Password does not match'
+            error = 'Password does not match';
+            return error;
         }
 
         try {
@@ -51,6 +54,9 @@
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Enter New Password</h2>
         <form class="mt-8 space-y-6" method="POST">
+            {#if error}  
+                <ListError error={error} />
+            {/if}
             <div>
                 <label for="password" class="sr-only">New Password</label>
                 <input id="password" bind:value={ password } name="password" type="password" autocomplete="email" required class="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="New Password">
